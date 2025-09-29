7ae953fbf054317328a92f9f5e77353c79e65bb6
Márkus Barnabás <markusbarnus10@gmail.com>
2025-09-29 12:32:59 +0200

    3.4 fejezet

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    function show($post) {
+        return view('postnezet', [
+            'postcim' => $post,
+            'posthossza' => Post::getLength($post)
+        ]);
+    }
+}
